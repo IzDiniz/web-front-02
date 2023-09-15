3821b89075ca4b14485f609cde8a9ded8935642a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 11:48:10 -0300

    Modificando arquivo Js

--- aula.js
+++ aula.js
@@ -1,1 +1,13 @@
+//Criando um alert em javascript
 alert('Olá mundo');
+
+
+//Adicionando umamensagem na tela
+document.write('Olá Mundo');
+
+//Adicionando console.log, usado apenas no console do inspecionar  
+console.log("Ola sou o Iago");
+
+//Criando uma variavel 
+let mensagem = "Olá, mundo"; 
+console.log("Mensagem de variável:", mensagem)
