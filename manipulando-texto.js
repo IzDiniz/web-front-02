0848bbedba282aede4f769d03b4e2782d93fdcc6
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 11:51:56 -0300

    Adicionando arquivo de manipulação de texto e ipos de dados

---
+++ manipulando-texto.js
@@ -0,0 +1,10 @@
+let numFloat = 123.543;
+let linguagens = ["Java", "C#", "Python"];
+let txt = "Estão chegando as provas";
+
+//funções nativas, são funções do java script, criadas no java script, que ja existe
+
+//console.log(typeof String(numFloat));
+//console.log(typeof linguagens);
+//console.log(linguagens[0]);
+console.log(txt.replace("provas", "Avaliações"));
---
+++ tipos-dados.js
@@ -0,0 +1,17 @@
+// int = numeros inteiros
+let idadeAluno = 18;
+
+// float = numeros flutuantes
+let valorProduto = 6.35;
+
+// strings - variaveis de textos, sempre por aspas duplas ou simples
+let nomeAluno = "Pedro Henrique";
+
+// boolean - recebe valores falsos e verdadeiros
+let casado = true; 
+
+//console.log(Typeof idadeAluno);
+//console.log(typeof valorProduto);
+//console.log(typeof nomeAluno);
+//console.log(typeof casado);
+//console.log(casado);
